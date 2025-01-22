b13b9b8aea5c8c7fb516ce67b74524b003428152
DIpanshu <dipanshu.magre@apliteinfo.com>
2025-01-22 12:23:43 +0530

    Company edit profile currections

--- app/src/main/java/com/app/bollyhood/fragment/editprofilefragments/CompanyEditProfileFragment.kt
+++ app/src/main/java/com/app/bollyhood/fragment/editprofilefragments/CompanyEditProfileFragment.kt
@@ -1,9 +1,18 @@
 package com.app.bollyhood.fragment.editprofilefragments
 
+import ImagePickerUtil
+import android.Manifest
+import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.text.Editable
 import android.text.SpannableStringBuilder
 import android.text.TextWatcher
@@ -18,6 +27,10 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -26,6 +39,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.app.bollyhood.R
 import com.app.bollyhood.activity.CMSActivity
+import com.app.bollyhood.activity.MyProfileActivity
+import com.app.bollyhood.activity.MyProfileActivity.Companion.REQUEST_ID_MULTIPLE_PERMISSIONS
 import com.app.bollyhood.adapter.WorkAdapter
 import com.app.bollyhood.databinding.FragmentCompanyEditProfileBinding
 import com.app.bollyhood.extensions.isNetworkAvailable
@@ -36,6 +51,7 @@ import com.app.bollyhood.extensions.isvalidName
 import com.app.bollyhood.extensions.isvalidTeamNCondition
 import com.app.bollyhood.model.ProfileModel
 import com.app.bollyhood.model.WorkLinkProfileData
+import com.app.bollyhood.util.PathUtils
 import com.app.bollyhood.util.PrefManager
 import com.app.bollyhood.util.StaticData
 import com.app.bollyhood.viewmodel.DataViewModel
@@ -47,6 +63,11 @@ import okhttp3.RequestBody
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @AndroidEntryPoint
 class CompanyEditProfileFragment : Fragment(), TextWatcher, WorkAdapter.onItemClick,
@@ -56,6 +77,8 @@ class CompanyEditProfileFragment : Fragment(), TextWatcher, WorkAdapter.onItemCl
     private val viewModel: DataViewModel by viewModels()
     lateinit var mContext: Context
     private var profilePath = ""
+    private var isCamera = false
+    private var isGallery = false
     private var category_Id: String = ""
     private var workLinkList: ArrayList<WorkLinkProfileData> = arrayListOf()
 
@@ -90,6 +113,8 @@ class CompanyEditProfileFragment : Fragment(), TextWatcher, WorkAdapter.onItemCl
         binding.apply {
             tvCompanyAddWorkLink.setOnClickListener(this@CompanyEditProfileFragment)
             tvUpdateProfile.setOnClickListener(this@CompanyEditProfileFragment)
+            ivBack.setOnClickListener(this@CompanyEditProfileFragment)
+            rrProfile.setOnClickListener(this@CompanyEditProfileFragment)
         }
     }
 
@@ -102,6 +127,18 @@ class CompanyEditProfileFragment : Fragment(), TextWatcher, WorkAdapter.onItemCl
 
             R.id.tvUpdateProfile->{
                 updateCompanyProfile()
+            }
+
+            R.id.ivBack ->{
+                (requireActivity() as? MyProfileActivity)?.closeActivity()
+            }
+
+            R.id.rrProfile->{
+                if (checkPermission()) {
+                    alertDialogForImagePicker()
+                } else {
+                    checkPermission()
+                }
             }
         }
     }
@@ -221,7 +258,7 @@ class CompanyEditProfileFragment : Fragment(), TextWatcher, WorkAdapter.onItemCl
     private fun setCompanyProfileData(profileModel: ProfileModel) {
         if (!profileModel.image.isNullOrEmpty()) {
             Glide.with(mContext).load(profileModel.image).error(R.drawable.ic_profile)
-                .error(R.drawable.ic_profile).into(requireActivity().findViewById(R.id.cvProfile))
+                .error(R.drawable.ic_profile).into(binding.cvProfile)
         }
 
         binding.edtName.setText(profileModel.name)
@@ -496,6 +533,98 @@ class CompanyEditProfileFragment : Fragment(), TextWatcher, WorkAdapter.onItemCl
                 getString(R.string.str_error_internet_connections),
                 Toast.LENGTH_SHORT
             ).show()
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        val permissionsNeeded = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            listOf(
+                Manifest.permission.CAMERA to ContextCompat.checkSelfPermission(mContext, Manifest.permission.CAMERA),
+                Manifest.permission.READ_MEDIA_IMAGES to ContextCompat.checkSelfPermission(mContext, Manifest.permission.READ_MEDIA_IMAGES)
+            )
+        } else {
+            listOf(
+                Manifest.permission.CAMERA to ContextCompat.checkSelfPermission(mContext, Manifest.permission.CAMERA),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE to ContextCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            )
+        }
+
+        val listPermissionsNeeded = permissionsNeeded
+            .filter { it.second != PackageManager.PERMISSION_GRANTED }
+            .map { it.first }
+
+        return if (listPermissionsNeeded.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(), listPermissionsNeeded.toTypedArray(), REQUEST_ID_MULTIPLE_PERMISSIONS
+            )
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun alertDialogForImagePicker() {
+        val dialogView = Dialog(requireContext())
+        dialogView.setContentView(R.layout.image_picker)
+        dialogView.setCancelable(false)
+        val txtcamera = dialogView.findViewById<TextView>(R.id.txtcamera)
+        val txtGallery = dialogView.findViewById<TextView>(R.id.txtGallery)
+        val txtCancel = dialogView.findViewById<TextView>(R.id.txtCancel)
+        txtcamera.setOnClickListener { v: View? ->
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startForProfileImageResult.launch(intent)
+
+            isCamera = true
+            isGallery = false
+            dialogView.dismiss()
+        }
+        txtGallery.setOnClickListener { v: View? ->
+            ImagePickerUtil.pickImageFromGallery(requireActivity(),startForProfileImageResult)
+            isCamera = false
+            isGallery = true
+            dialogView.dismiss()
+        }
+        txtCancel.setOnClickListener { v: View? -> dialogView.dismiss() }
+        dialogView.show()
+    }
+
+    private val startForProfileImageResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            val resultCode = result.resultCode
+            val data = result.data
+
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    if (isCamera) {
+                        val imageBitmap = result.data?.extras?.get("data") as Bitmap
+                        profilePath = saveImageToStorage(imageBitmap).toString()
+                        binding.cvProfile.setImageURI(Uri.parse(profilePath))
+                    } else if (isGallery) {
+                        val uri = data!!.data
+                        profilePath = PathUtils.getRealPath(requireContext(), uri!!).toString()
+                        binding.cvProfile.setImageURI(uri)
+                    }
+                }
+                else -> {
+                    Toast.makeText(requireContext(), "Task Cancelled", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    private fun saveImageToStorage(bitmap: Bitmap): String? {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val imageFileName = "JPEG_$timeStamp.jpg"
+        val storageDir: File? = mContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        return try {
+            val imageFile = File(storageDir, imageFileName)
+            val fos = FileOutputStream(imageFile)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, fos)
+            fos.close()
+            imageFile.absolutePath
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
         }
     }
 }
