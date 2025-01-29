f89297f9db122f62c21168db0c47fab569790118
DIpanshu <dipanshu.magre@apliteinfo.com>
2025-01-29 16:40:10 +0530

    Ui design for allBookMark

--- app/src/main/java/com/app/bollyhood/activity/MyBookMarkActivity.kt
+++ app/src/main/java/com/app/bollyhood/activity/MyBookMarkActivity.kt
@@ -2,12 +2,13 @@ package com.app.bollyhood.activity
 
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import com.app.bollyhood.R
 import com.app.bollyhood.adapter.BookMarkAdapter
 import com.app.bollyhood.databinding.ActivityMyBookMarkBinding
@@ -19,12 +20,14 @@ import com.app.bollyhood.viewmodel.DataViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MyBookMarkActivity : AppCompatActivity(), BookMarkAdapter.onItemClick {
+class MyBookMarkActivity : AppCompatActivity(), BookMarkAdapter.onItemClick,OnClickListener {
 
     lateinit var binding: ActivityMyBookMarkBinding
     lateinit var mContext: MyBookMarkActivity
     private val viewModel: DataViewModel by viewModels()
     private val bookMarkList: ArrayList<BookMarkModel> = arrayListOf()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_my_book_mark)
@@ -51,9 +54,7 @@ class MyBookMarkActivity : AppCompatActivity(), BookMarkAdapter.onItemClick {
     }
 
     private fun addListner() {
-        binding.ivBack.setOnClickListener {
-            finish()
-        }
+        binding.ivBack.setOnClickListener(this)
     }
 
     private fun addObserevs() {
@@ -73,9 +74,7 @@ class MyBookMarkActivity : AppCompatActivity(), BookMarkAdapter.onItemClick {
                     binding.tvNoFavourites.visibility = View.GONE
                     binding.rvBookMark.visibility = View.VISIBLE
                     setAdapter(bookMarkList)
-
                 } else {
-
                     binding.tvNoFavourites.visibility = View.VISIBLE
                     binding.rvBookMark.visibility = View.GONE
                 }
@@ -88,7 +87,7 @@ class MyBookMarkActivity : AppCompatActivity(), BookMarkAdapter.onItemClick {
 
     private fun setAdapter(bookMarkList: ArrayList<BookMarkModel>) {
         binding.apply {
-            rvBookMark.layoutManager = LinearLayoutManager(mContext)
+            rvBookMark.layoutManager = GridLayoutManager(mContext, 2)
             rvBookMark.setHasFixedSize(true)
             adapter = BookMarkAdapter(mContext, bookMarkList, this@MyBookMarkActivity)
             rvBookMark.adapter = adapter
@@ -104,5 +103,13 @@ class MyBookMarkActivity : AppCompatActivity(), BookMarkAdapter.onItemClick {
                 Gson().toJson(model)
             )
         )*/
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.ivBack  ->{
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/com/app/bollyhood/adapter/BookMarkAdapter.kt
+++ app/src/main/java/com/app/bollyhood/adapter/BookMarkAdapter.kt
@@ -1,13 +1,11 @@
 package com.app.bollyhood.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.app.bollyhood.activity.MyBookMarkActivity
 import com.app.bollyhood.databinding.AdpBookmarkBinding
 import com.app.bollyhood.model.BookMarkModel
-import com.bumptech.glide.Glide
 
 class BookMarkAdapter(
     val mContext: MyBookMarkActivity,
@@ -32,21 +30,6 @@ class BookMarkAdapter(
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val model = bookMarkList[position]
-        holder.binding.tvName.setText(model.name)
-        if (model.image.isNotEmpty()) {
-            Glide.with(mContext).load(bookMarkList[position].image)
-                .into(holder.binding.ivImage)
-        }
-
-        if (bookMarkList[position].is_verify == "1") {
-            holder.binding.ivBookMark.visibility = View.VISIBLE
-        } else {
-            holder.binding.ivBookMark.visibility = View.GONE
-        }
-
-        holder.itemView.setOnClickListener {
-            onclick.onClick(position, bookMarkList[position])
-        }
     }
 
     class MyViewHolder(val binding: AdpBookmarkBinding) : RecyclerView.ViewHolder(binding.root)
